895c3a5accfa42c6ab5b9098b1161513c7e9d69e
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-10-17 20:27:51 +1100

    updated with modal

--- koori-app/public/script.js
+++ koori-app/public/script.js
@@ -1,4 +1,28 @@
-function scrollToSection(id) {
-  const element = document.getElementById(id);
-  element.scrollIntoView({ behavior: 'smooth' });
+// Add smooth scrolling to all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Show acknowledgment modal on load
+window.onload = function() {
+var modal = document.getElementById('ackModal');
+var span = document.getElementsByClassName('close')[0];
+
+modal.style.display = 'block';
+
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
 }
